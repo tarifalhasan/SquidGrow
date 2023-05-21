ebd7509c758e1274c7acf973a2ffb7ac607879dc
tarifalhasan <tarialhasan999@gmail.com>
2023-05-21 20:23:45 +0600

    Tradein

---
+++ Tradein/app/page.tsx
@@ -0,0 +1,12 @@
+import FooterDesktop from '@/components/FooterDesktop';
+import Hero from '@/components/hero/Hero';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <FooterDesktop />
+    </>
+  );
+}
